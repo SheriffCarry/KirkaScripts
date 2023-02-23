6e436c969cc20ddc1fbe6260e68ed5686acc8d6a
SheriffCarry <125906244+SheriffCarry@users.noreply.github.com>
2023-02-23 06:47:08 +0100

    Create lefthandtoggle.js

---
+++ Userscript/lefthandtoggle.js
@@ -0,0 +1,17 @@
+//Find the code at https://www.toptal.com/developers/keycode (event.key)
+let key = ""
+//Find the code at https://www.toptal.com/developers/keycode (event.key)
+
+let response
+let text
+async function start() {
+    response = await fetch("https://raw.githubusercontent.com/SheriffCarry/KirkaScripts/main/Running%20Code/csstoggle.js")
+    text = await response.text();
+    eval(text)
+}
+document.addEventListener("DOMContentLoaded", () => {
+  if (key !== "") {
+    window.lefhandtoggle = key
+  }
+})
+start()
