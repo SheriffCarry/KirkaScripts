186948dfd13f96a59792a0b6f990be55f3b3f92f
SheriffCarry <125906244+SheriffCarry@users.noreply.github.com>
2023-03-02 07:09:07 +0100

    Create questinfo.js

---
+++ Userscript/questinfo.js
@@ -0,0 +1,8 @@
+let response
+let text
+async function start() {
+    response = await fetch("https://raw.githubusercontent.com/SheriffCarry/KirkaScripts/main/Running%20Code/questinfo.js")
+    text = await response.text();
+    eval(text)
+}
+start()
