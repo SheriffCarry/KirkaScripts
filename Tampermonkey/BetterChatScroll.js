aa6a6c235b0d7aa985e7581c149c459d27e518fe
SheriffCarry <125906244+SheriffCarry@users.noreply.github.com>
2023-03-29 20:58:54 +0200

    Create BetterChatScroll.js

---
+++ Tampermonkey/BetterChatScroll.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         BetterChatScroll
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *://kirka.io/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=kirka.io
+// @grant        none
+// ==/UserScript==
+
+let scrollspeed = 10;
+
+
+let response
+let text
+async function start() {
+    response = await fetch("https://raw.githubusercontent.com/SheriffCarry/KirkaScripts/main/Running%20Code/BetterChatScroll.js")
+    text = await response.text();
+    eval(text)
+}
+start()
