d78a4661ec5c7fe4a0a4996684d202e6f7d9cf2e
SheriffCarry <125906244+SheriffCarry@users.noreply.github.com>
2023-03-29 20:59:49 +0200

    Create InventorySearch.js

---
+++ Tampermonkey/InventorySearch.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         InventorySearch
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *://kirka.io/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=kirka.io
+// @grant        none
+// ==/UserScript==
+
+let response
+let text
+async function start() {
+    response = await fetch("https://raw.githubusercontent.com/SheriffCarry/KirkaScripts/main/Running%20Code/InventorySearch.js")
+    text = await response.text();
+    eval(text)
+}
+start()
