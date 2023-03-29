f60fd7d468e729e6d0a3e7d2826007f4b2956cfc
SheriffCarry <125906244+SheriffCarry@users.noreply.github.com>
2023-03-29 20:55:51 +0200

    Create 0AdsToggle.js

---
+++ Tampermonkey/0AdsToggle.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         0AdsToggle
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *://kirka.io/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=kirka.io
+// @grant        none
+// ==/UserScript==
+
+//Find the code at https://www.toptal.com/developers/keycode (event.key)
+let key = ""
+//Find the code at https://www.toptal.com/developers/keycode (event.key)
+
+
+let response
+let text
+async function start() {
+    response = await fetch("https://raw.githubusercontent.com/SheriffCarry/KirkaScripts/main/Running%20Code/0AdsToggle.js")
+    text = await response.text();
+    eval(text)
+}
+start()
