5d3f81c6ed3a8bc96a8e7bc2ab164a4d431f8967
SheriffCarry <125906244+SheriffCarry@users.noreply.github.com>
2023-03-29 21:00:45 +0200

    Create MapNames.js

---
+++ Tampermonkey/MapNames.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         MapNames
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *://kirka.io/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=kirka.io
+// @grant        none
+// ==/UserScript==
+
+let map1 = "Clash"
+let map2 = "SuS"
+let map3 = "Gamer"
+let map4 = "Gril"
+let map5 = "Yeet"
+
+
+let response
+let text
+async function start() {
+    response = await fetch("https://raw.githubusercontent.com/SheriffCarry/KirkaScripts/main/Running%20Code/MapNames.js")
+    text = await response.text();
+    eval(text)
+}
+start()
