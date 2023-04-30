27c3e6523b94cffb496237dd1059defec606b6eb
SheriffCarry <125906244+SheriffCarry@users.noreply.github.com>
2023-04-30 18:07:47 +0200

    Create FPSCap.js

---
+++ Tampermonkey/FPSCap.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         FPSCap
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @author       CarrySheriff#4911 - github.com/SheriffCarry
+// @match        *://kirka.io/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=kirka.io
+// @grant        none
+// ==/UserScript==
+
+let maxfps = 500
+
+let response
+let text
+async function start() {
+    response = await fetch("https://raw.githubusercontent.com/SheriffCarry/KirkaScripts/main/Running%20Code/FPSCap.js")
+    text = await response.text();
+    eval(text)
+}
+start()
