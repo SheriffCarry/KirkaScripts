e852de91ecea8b24d174fe892b8054a451da6bee
SheriffCarry <twitchsheriffcarry+sheriffcarrygithub@gmail.com>
2024-01-02 21:22:44 +0100

    Create WordMode.js

---
+++ Tampermonkey/WordMode.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         WordMode
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @author       CarrySheriff#4911 - github.com/SheriffCarry
+// @match        *://kirka.io/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=kirka.io
+// @grant        none
+// ==/UserScript==
+
+let response
+let text
+async function start() {
+    response = await fetch("https://raw.githubusercontent.com/SheriffCarry/KirkaScripts/main/Running%20Code/WordMode.js")
+    text = await response.text();
+    eval(text)
+}
+start()
