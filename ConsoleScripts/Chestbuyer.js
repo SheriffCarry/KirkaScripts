3f2411ff171ffb53f597646326c578f2803efcfc
SheriffCarry <twitchsheriffcarry+sheriffcarrygithub@gmail.com>
2024-02-29 22:37:48 +0100

    Update Chestbuyer.js

--- ConsoleScripts/Chestbuyer.js
+++ ConsoleScripts/Chestbuyer.js
@@ -1,15 +1,17 @@
-let amount = 1;
-let chest = "1"; //1 = Wood, 2 = Ice, 3 = Golden
-for (let index = 0; index < amount; index++) {
-  let req = await fetch("https://api.kirka.io/api/shop/buy", {
-    headers: {
-      accept: "application/json, text/plain, */*",
-      authorization: `Bearer ${localStorage.token}`,
-      "content-type": "application/json;charset=UTF-8",
-    },
-    body: `{\"id\":${chest}}`,
-    method: "POST",
-  });
-  req = await req.status;
-  //console.log(req)
-}
+(async () => {
+  let amount = 1;
+  let chest = "1"; //1 = Wood, 2 = Ice, 3 = Golden
+  for (let index = 0; index < amount; index++) {
+    let req = await fetch("https://api.kirka.io/api/shop/buy", {
+      headers: {
+        accept: "application/json, text/plain, */*",
+        authorization: `Bearer ${localStorage.token}`,
+        "content-type": "application/json;charset=UTF-8",
+      },
+      body: `{\"id\":${chest}}`,
+      method: "POST",
+    });
+    req = await req.status;
+    //console.log(req)
+  }
+})();
