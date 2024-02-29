be9055ea3491250d2a92f24350a3b27beae828d3
SheriffCarry <twitchsheriffcarry+sheriffcarrygithub@gmail.com>
2024-02-29 22:37:29 +0100

    Update Cardbuyer.js

--- ConsoleScripts/Cardbuyer.js
+++ ConsoleScripts/Cardbuyer.js
@@ -1,15 +1,17 @@
-let amount = 1;
-let card = "1"; //4 = Party, 5 = Soldiers, 6 = GirlsBand, 30 = Cold
-for (let index = 0; index < amount; index++) {
-  let req = await fetch("https://api.kirka.io/api/shop/buy", {
-    headers: {
-      accept: "application/json, text/plain, */*",
-      authorization: `Bearer ${localStorage.token}`,
-      "content-type": "application/json;charset=UTF-8",
-    },
-    body: `{\"id\":${card}}`,
-    method: "POST",
-  });
-  req = await req.status;
-  //console.log(req)
-}
+(async () => {
+  let amount = 1;
+  let card = "1"; //4 = Party, 5 = Soldiers, 6 = GirlsBand, 30 = Cold
+  for (let index = 0; index < amount; index++) {
+    let req = await fetch("https://api.kirka.io/api/shop/buy", {
+      headers: {
+        accept: "application/json, text/plain, */*",
+        authorization: `Bearer ${localStorage.token}`,
+        "content-type": "application/json;charset=UTF-8",
+      },
+      body: `{\"id\":${card}}`,
+      method: "POST",
+    });
+    req = await req.status;
+    //console.log(req)
+  }
+})();
